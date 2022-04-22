cf08c8d183e1daf9b54a2f6628c34f07ab0e09ab
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2022-04-22 11:30:17 +0200

    delete scriipt

--- delete.ps1
+++ delete.ps1
@@ -1,88 +1,91 @@
 $config = ConvertFrom-Json $configuration
-
+$updateOnly = $config.updateOnly
 $BaseUri = $config.BaseUri
-$Token = $config.Token
-$RelationNumber = $config.RelationNumber
-$updateUserId = $config.updateUserId
+$token = $config.Token
 $getConnector = "T4E_HelloID_Users"
 $updateConnector = "knUser"
 
 #Initialize default properties
-$p = $person | ConvertFrom-Json;
-$m = $manager | ConvertFrom-Json;
-$aRef = $accountReference | ConvertFrom-Json;
-$mRef = $managerAccountReference | ConvertFrom-Json;
-$success = $False;
-$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+$p = $person | ConvertFrom-Json
+$aRef = $accountReference | ConvertFrom-Json
+$success = $False
+$auditLogs = New-Object Collections.Generic.List[PSCustomObject]
 
 # Set TLS to accept TLS, TLS 1.1 and TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
-$filterfieldid = "Gebruiker"
-$filtervalue = $aRef.Gebruiker; # Has to match the AFAS value of the specified filter field ($filterfieldid)
+$personId = $p.ExternalId # Profit Person number (which could be different to the Profit Employee number)
 
-$currentDate = (Get-Date).ToString("dd/MM/yyyy hh:mm:ss")
+#emailadres + upn wijzigingen.
+$emailaddress = $null # or "$personId@customer.nl" # Unique value based of PersonId because at the revoke action we want to clear the unique fields.
+$userPrincipalName = $null # or "$personId@customer.nl" # Unique value based of PersonId because at the revoke action we want to clear the unique fields
 
 try{
-    $encodedToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Token))
-    $authValue = "AfasToken $encodedToken"
-    $Headers = @{ Authorization = $authValue }
-    $getUri = $BaseUri + "/connectors/" + $getConnector + "?filterfieldids=$filterfieldid&filtervalues=$filtervalue&operatortypes=1"
-    $getResponse = Invoke-RestMethod -Method Get -Uri $getUri -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
+    if ($updateOnly -eq $false){
+        $encodedToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($token))
+        $authValue = "AfasToken $encodedToken"
+        $Headers = @{ Authorization = $authValue }
 
-    if($getResponse.rows.Count -eq 1 -and (![string]::IsNullOrEmpty($getResponse.rows.Gebruiker))){     
-        # Map the account data
-        $account = [PSCustomObject]@{
-            'KnUser' = @{
-                'Element' = @{
-                    '@UsId' = $getResponse.rows.Gebruiker;
-                    'Fields' = @{
-                        # Mutatie code
-                        'MtCd' = 2;
-                        # Omschrijving
-                        "Nm" = "Deleted by HelloID Provisioning";
+        $getUri = $BaseUri + "/connectors/" + $getConnector + "?filterfieldids=Persoonsnummer&filtervalues=$personId&operatortypes=1"
+        $getResponse = Invoke-RestMethod -Method Get -Uri $getUri -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
+
+        if($getResponse.rows.Count -eq 1 -and (![string]::IsNullOrEmpty($getResponse.rows.Gebruiker))){
+
+            # Change mapping here
+            $account = [PSCustomObject]@{
+                'KnUser' = @{
+                    'Element' = @{
+                        '@UsId' = $getResponse.rows.Gebruiker
+                        'Fields' = @{
+                            # Mutatie code
+                            'MtCd' = 2
+                            # Omschrijving
+                            "Nm" = "Deleted by HelloID Provisioning"
+                            # E-mail
+                           'EmAd'  = $emailaddress
+                            # UPN
+                            'Upn' = $userPrincipalName
+                        }
                     }
                 }
             }
-        }
 
-        if(-Not($dryRun -eq $True)){
-            $deleteUri = $BaseUri + "/connectors/" + $updateConnector + "/KnUser/@UsId,MtCd,GrId,GrDs,BcCo,UsIdNew,Nm,Awin,Acon,Abac,Acom,Site,EmAd,XOEA,InSi,Upn,InLn,OcUs,PoMa,AcUs/$($account.knUser.Values.'@UsId'),,,,,$($account.knUser.Values.Fields.MtCd),$($account.knUser.Values.Fields.Nm),false,false,false,false,false,,,false,,,false,false,false"
-            $deleteResponse = Invoke-RestMethod -Method DELETE -Uri $deleteUri -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
+            if(-Not($dryRun -eq $True)){  
+                $body = $account | ConvertTo-Json -Depth 10
+                $putUri = $BaseUri + "/connectors/" + $updateConnector
+                $auditLogs.Add([PSCustomObject]@{
+                    Action = "DeleteAccount"
+                    Message = "Deleted account with Id $($aRef)"
+                    IsError = $false
+                })
+                $null = Invoke-RestMethod -Method Put -Uri $putUri -Body $body -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing -ErrorAction Stop
+
+                $success = $true
+            }
         }
-        
+    }else{
         $auditLogs.Add([PSCustomObject]@{
             Action = "DeleteAccount"
-            Message = "Deleted account with Id $($aRef.Gebruiker)"
-            IsError = $false;
-        });
+            Message = "Deleted account with Id $($aRef)"
+            IsError = $false
+        })
 
-        $success = $true;          
+        $success = $true
     }
-    else {
-        $auditLogs.Add([PSCustomObject]@{
-            Action = "DeleteAccount"
-            Message = "No profit user found for person $filtervalue";
-            IsError = $false;
-        });        
-
-        $success = $true;         
-        Write-Warning "No profit user found for person $filtervalue";
-    }    
 }catch{
     $auditLogs.Add([PSCustomObject]@{
         Action = "DeleteAccount"
-        Message = "Error deleting account with Id $($aRef.Gebruiker): $($_)"
+        Message = "Error deleting account with Id $($aRef): $($_)"
         IsError = $True
-    });
-    Write-Warning $_;
+    })
+	Write-Verbose -Verbose "$_"
 }
 
 # Send results
 $result = [PSCustomObject]@{
-	Success= $success;
-	AccountReference= $aRef;
-	AuditLogs = $auditLogs;
-    Account = $account;
-};
-Write-Output $result | ConvertTo-Json -Depth 10;
+	Success= $success
+	AccountReference= $aRef
+	AuditLogs = $auditLogs
+    Account = $account
+}
+Write-Output $result | ConvertTo-Json -Depth 10
