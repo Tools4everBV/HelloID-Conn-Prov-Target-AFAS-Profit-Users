96f7076c7d135101ae8b8d744acee71340996c2d
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-09-08 14:02:17 +0200

    Update delete.ps1

--- delete.ps1
+++ delete.ps1
@@ -145,7 +145,7 @@ try {
         $propertiesChanged += @('EmAd')
     }
     if ($propertiesChanged) {
-        Write-Verbose "Account property(s) required to update: [$($propertiesChanged.name -join ",")]"
+        Write-Verbose "Account property(s) required to update: [$($propertiesChanged -join ",")]"
         $updateAction = 'Update'
     }
     else {
